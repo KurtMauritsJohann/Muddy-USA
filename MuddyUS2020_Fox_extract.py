eabd831c310a96218d9d6195016a39e8bdd9a7bd
KurtMauritsJohann <67815806+KurtMauritsJohann@users.noreply.github.com>
2023-12-02 14:43:12 -0800

    Add files via upload

--- MuddyUS2020_Fox_extract.py
+++ MuddyUS2020_Fox_extract.py
@@ -1,14 +1,72 @@
 #MuddyUS2020_Fox_extract
 
-from requests import get
-url = 'https://www.foxnews.com/elections/2020/general-results'
-response = get(url)
-# print(response.text[:250])
+# Fox doesn't report results of write-in candidates; only those listed on ballot
 
-tree1 = html.fromstring(response.content)
-states = tree1.xpath('//div[@class="content state-list"]//div[@class="state"]//a/@href')
-# for st in state:
+from requests import get
+from lxml import html
+import numpy as np
+import pandas as pd
+
+domain = 'https://www.foxnews.com'
+url = '/elections/2020/general-results'
+response = get(domain + url)
+
+tree = html.fromstring(response.content)
+states = tree.xpath('//div[@class="content state-list"]//div[@class="state"]//a/@href')
+full_US = []
+for st in states:
     #do all the things
+    #start from url
+    #go to each state's individual page
+    source = get(domain + st)
+    #source = get('https://www.foxnews.com/elections/2020/general-results/state/california')
+    tree1 = html.fromstring(source.content)
+    #pull all parties, candidates, votes, percentages
+    test = tree1.xpath('//div[@data-table="race-table-president-county"]//span[1]/text()')
+    #pull all county names into a separate list
+    cty = tree1.xpath('//div[@data-table="race-table-president-county"]//div[@class="content race-table"]//span[@class="county"]/text()')
+    fips = tree1.xpath('//div[@data-table="race-table-president-county"]//div[@class="content race-table"]//table/@data-county-fips')
+
+    tab = [item for item in test if item not in ['In', 'CLOSED', ' Polls are closed']]
+    tab = [item for item in tab if item not in cty]
+    mat = []
+
+    while tab != []:
+        mat.append(tab[:4])
+        tab = tab[4:]
+
+    mat = np.asarray(mat)
+    mat = pd.DataFrame(mat)
+    
+    cty = cty * int(len(mat) / len(cty))
+    cty.sort()
+    fips = fips * int(len(mat) / len(fips))
+    fips.sort()
+
+    mat[4] = cty
+    mat[5] = fips
+    mat[6] = st.rsplit('/', 1)[-1].replace('-', ' ').title() #state name only, title case
+    mat = mat.reindex(columns=[5, 6, 4, 0, 1, 2, 3])
+    mat.columns = ['FIPS', 'State', 'County', 'Party', 'Candidate', 'Votes', 'Percent']
+
+    mat = mat.astype('string')
+    mat['Votes'] = mat['Votes'].str.replace(',', '').astype(int)
+
+    #append st to list of dataframes
+    full_US.append(mat)
+# export list to csv
+full_US.to_csv('~/home/nathan/Documents/FOXMuddyUS2020.csv', index=False)
+
+#extract data for:
+#   all states
+#   all counties
+#   all candidates
+#   all votes
+#   all percentage
+
+
+
+
 
 # from bs4 import BeautifulSoup
 # html_soup = BeautifulSoup(response.text, 'html.parser')
@@ -38,19 +96,6 @@ states = tree1.xpath('//div[@class="content state-list"]//div[@class="state"]//a
 # cont[0].find_all('span', {'class': 'count'})[0].text.strip() # = '617,659'
 # cont[0].find_all('span', {'class': 'percent'})[0].text.strip() # = '80.20%'
 
-#start from url
-#go to each state's individual page
-#extract data for:
-#   county/fips?
-#   all candidates (test case: Biden, Trump)
-#   all votes (test case: Biden, Trump)
-#   all percentage (test case: Biden, Trump)
-
-
-from lxml import html
-source = get('https://www.foxnews.com/elections/2020/general-results/state/california')
-# Fox doesn't report results of write-in candidates; only those listed on ballot
-tree = html.fromstring(source.content)
 # county = tree.xpath('//div[@data-table="race-table-president-county"]//span[@class="county"]/text()')
 # candidate = tree.xpath('//div[@data-table="race-table-president-county"]//span[@class="is-long"]/text()')
 # for i in range(len(county)):
@@ -63,18 +108,9 @@ tree = html.fromstring(source.content)
 # county1 = county * int(len(candidate)/len(county))
 # county1.sort()
 
-import pandas as pd
 # ca2020 = pd.DataFrame([county1, candidate, votes, percent])
 # ca2020 = ca2020.T
 # ca2020.columns = ['County', 'Candidate', 'Votes', 'Percent']
-
-test = tree.xpath('//div[@data-table="race-table-president-county"]//span[1]/text()')
-cty1 = tree.xpath('//div[@class="content race-table"]//span[@class="county"]/text()')
-
-for i in range(58):
-    test.remove('In')
-    test.remove('CLOSED')
-    test.remove(' Polls are closed')
 
 #generalize 25 step (n candidates * 4 values (party, name, votes, pct) + 1 county)
 # testlist = []
@@ -99,30 +135,23 @@ for i in range(58):
 
 
 
-#Can I pull all county names out into a separate list first? 
-[item for item in test if item not in cty1]
 #Then pulling each remaining column out should be much easier
-test5 = []
-for j in range(4):
-    test3 = []
-    for i in range(58):
-        test3.append(test[(i * 25 + j + 1):((i + 1) * 25):4])
-    test4 = []
-    for sublist in test3:
-        for item in sublist:
-            test4.append(item)
-    test5.append(test4)
+# test5 = []
+# for j in range(4):
+#     test3 = []
+#     for i in range(58):
+#         test3.append(test[(i * 25 + j + 1):((i + 1) * 25):4])
+#     test4 = []
+#     for sublist in test3:
+#         for item in sublist:
+#             test4.append(item)
+#     test5.append(test4)
 
-testdf1 = pd.DataFrame(test5).T
+# testdf1 = pd.DataFrame(test5).T
 
-cty = test[::25] * 6
-cty.sort()
+# cty = test[::25] * 6
+# cty.sort()
 
-testdf1['County'] = cty
-testdf1 = testdf1.reindex(columns=['County', 0, 1, 2, 3])
-testdf1.columns = ['County', 'Party', 'Candidate', 'Votes', 'Percent']
-
-testdf1 = testdf1.astype('string')
-testdf1['Votes'] = testdf1['Votes'].str.replace(',', '').astype(int)
-
-testdf1.to_csv('~/home/nathan/Documents/FOXMuddyCA2020.csv', index=False)
+# testdf1['County'] = cty
+# testdf1 = testdf1.reindex(columns=['County', 0, 1, 2, 3])
+# testdf1.columns = ['County', 'Party', 'Candidate', 'Votes', 'Percent']
